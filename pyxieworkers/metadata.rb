40976687dacc811774c4413dedc20789ae60b631
Andrew Odewahn <odewahn@oreilly.com>
2013-11-15 14:04:57 -0500

    adding metadata

--- pyxieworkers/metadata.rb
+++ pyxieworkers/metadata.rb
@@ -5,7 +5,8 @@ license          'MIT'
 description      'Installs/Configures pyxie-workers'
 long_description 'Installs/Configures pyxie-workers'
 version          '0.1.0'
-recipe           'pyxieworkers', 'Installs and configures pyxie workers'
+recipe           'pyxieworkers::deploy', 'Installs and configures pyxie workers'
+recipe           'pyxieworkers::configure_localhost', 'Installs and configures pyxie workers on a vagrant box'
 
 
 %w[ debian ubuntu centos redhat fedora scientific suse amazon].each do |os|
